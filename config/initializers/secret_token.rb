c057812d435a86d20719707025a0995f27704b51
isanderus <isanderus@gmail.com>
2014-02-16 00:58:35 +0100

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rorpractise::Application.config.secret_key_base = 'c0fc86a16d19cdcef12d47d782eb9357855e48ec687b9a695a4fc5373d1c92861358578e6d5dd4b4d4e2aa6c5bec70c3996e1d795889b32a4b997240f4122635'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rorpractise::Application.config.session_store :cookie_store, key: '_rorpractise_session'
